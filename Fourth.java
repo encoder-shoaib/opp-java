e18ac685c5937312e70c8c236dcc42a66f609ec4
shoaib-ahammde <ahammed15-4955@diu.edu.bd>
2023-02-02 23:55:25 +0600

    shoaib ahammed

---
+++ Fourth.java
@@ -0,0 +1,15 @@
+// Find the area of a Circle. [Print 2 digits after decimal point].
+
+import java.util.*;
+public class Fourth {
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the radius of circle =");
+        int radius  =sc.nextInt();
+        double area = radius *radius*3.1416;
+        System.out.println("Area="+String.format("%.2f",area));
+
+
+    }
+}
